65eec8abea613b3e6f3e06cbac79626f9592127a
DavidCydeo <david@cydeo.com>
2023-12-13 02:43:57 +0300

    Day01 Executed

---
+++ src/Day01_Intro/PrintPractice.java
@@ -0,0 +1,21 @@
+package Day01_Intro;
+
+public class PrintPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cydeo");
+
+        System.out.println();
+
+        System.out.println("Hello David");
+
+        System.out.print("one");
+        System.out.print("two");
+        System.out.print("three");
+
+
+    }
+
+
+}
