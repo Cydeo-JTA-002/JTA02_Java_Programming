0dc543cdb79134729a3bd10c27b1a564918199de
DavidCydeo <david@cydeo.com>
2024-01-02 21:04:37 +0300

    Day08 Executed

---
+++ src/Day08_IfStatements/SingleIf.java
@@ -0,0 +1,33 @@
+package Day08_IfStatements;
+
+public class SingleIf {
+
+    public static void main(String[] args) {
+
+        int num = -10;
+
+        if (num > 0) {
+
+            System.out.println(num + " is a positive number");
+       }
+
+        System.out.println("I am out of if code block");
+        System.out.println("continue coding");
+
+
+        System.out.println("*************************************");
+// CydeoJR age limit is 12-17
+
+        int age=5;
+
+        if (age>=12 && age<=17) {
+
+            System.out.println("You can join CydeoJR programs");
+        }
+
+        System.out.println("I am out of if code block");
+        System.out.println("continue coding");
+
+    }
+
+}
